93f3624a3b54429cbb7cedbf376b27c311dfbd79
jeehwan <tjtojan@naver.com>
2022-04-10 00:18:07 +0900

    cookie and session add

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ const express =require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+const bcrypt = require('bcrypt');
+const saltRounts = 10;
+
 const app = express();
 
 app.use(express.json());
@@ -12,6 +19,18 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(cookieParser);
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(session({
+    key : "userId",
+    secret : "subscribe",
+    resave : false,
+    saveUninitialized: false,
+    cookie : {
+        expires : 60 * 60 * 24,
+    }
+}));
 
 const db = mysql.createConnection({
     user:'root',
@@ -25,11 +44,18 @@ app.post('/register', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    db.query('Insert into users (username, password) values (?, ?)', 
-    [username, password], 
-    (err, result) => {
-        console.log(err)
-    });
+    bcrypt.hash(password, saltRounts, (err, hash) => {
+
+        if(err) {
+            console.log(err);
+        }
+        db.query('Insert into users (username, password) values (?, ?)', 
+        [username, hash], 
+        (err, result) => {
+            console.log(err)
+        });
+
+    })
 });
 
 app.post('/login', (req, res) => {
@@ -37,18 +63,26 @@ app.post('/login', (req, res) => {
     const password = req.body.password;
 
     db.query(
-        "SELECT * from users where username = ? and password = ?",
-        [username, password],
+        "SELECT * from users where username = ?",
+        [username],
         (err, result) => {
             if(err) {
                 res.send({err : err});
             }
 
             if(result.length > 0) {
-                 res.send(result);
+                 bcrypt.compare(password, result[0].password, (err, response) => {
+                    if(response) {
+                        req.session.user = result;
+                        console.log(req.session.user);
+                        res.send(result)
+                    } else {
+                        res.send({message : "Wrong usernme/password combination!"});
+                    }
+                 })
             }
             else {
-                res.send( {message : "Wrong username/password combinations"});
+                res.send( {message : "User doesn't exist"})
             }
         }
     );
